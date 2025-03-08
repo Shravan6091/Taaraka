eb23ea47d27a379e3647b46ba2739e00201efe02
Shravan6091 <shravan.venumula@gmail.com>
2025-03-08 14:40:57 +0400

    Delete Server directory

--- Server/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { processPayment } = require('../controllers/paymentController');
-
-const router = express.Router();
-
-router.post('/process', processPayment);
-
-module.exports = router;
--- Server/routes/projectRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProjects, getProjectById } = require('../controllers/projectController');
-
-const router = express.Router();
-
-router.get('/projects', getProjects);
-router.get('/projects/:id', getProjectById);
-
-module.exports = router;
